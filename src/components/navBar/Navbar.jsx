47f424503f1c22c4d66c60058b4fa563239f52ee
AhmedGhonim1998 <146675736+AhmedGhonim1998@users.noreply.github.com>
2024-06-20 22:38:35 +0300

    update

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,13 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef,useEffect } from 'react';
 import cryptoLogo from '../../assets/cryptosphere.png';
 import { Button, Row, Col } from 'react-bootstrap';
 import { MdOutlineArrowOutward } from "react-icons/md";
-import { NavLink } from 'react-router-dom';
+import { NavLink ,Link} from 'react-router-dom';
 import { CoinContext } from '../../assets/context/CoinContext'; // Ensure the path is correct
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import * as AiIcons from 'react-icons/ai';
+import { SideNavData } from './SideBar';
 export default function Navbar() {
     const { setCurrency } = useContext(CoinContext);
+    const [sidebar, setSidebar] = useState(false);
+    let menuRef = useRef();
+    useEffect(() => {
+        let sidebarHandler = (e) => {
+            if (!menuRef.current.contains(e.target)) {
+                setSidebar(false);
+                console.log(menuRef.current);
+            }
+        };
 
+        document.addEventListener("mousedown", sidebarHandler);
+
+
+        return () => {
+            document.removeEventListener("mousedown", sidebarHandler);
+        }
+
+    });
+    const toggleSidebar = () => {
+        setSidebarVisible(!sidebarVisible);
+    };
     const currencyHandler = (e) => {
         switch (e.target.value) {
             case "usd":
@@ -43,18 +65,47 @@ export default function Navbar() {
                         </ul>
                     </Col>
                     <Col lg={4} md={4} sm={4} xs={4} className='my-auto'>
-                        <div className="navRightFlex d-flex justify-content-end flex-column">
-                            <div className="navRight">
-                                <select className='me-2' onChange={currencyHandler}>
-                                    <option value="usd" className='text-uppercase'>usd</option>
-                                    <option value="eur" className='text-uppercase'>eur</option>
-                                    <option value="inr" className='text-uppercase'>inr</option>
-                                </select>
-                                <Button variant="primary" className='text-capitalize signUp'>sign up <MdOutlineArrowOutward />
-                                </Button>
-                            </div>
-                        </div>
+                        <Row>
+                            <Col lg={12} md={10} sm={10} xs={10}>
+                                <div className="navRightFlex d-flex justify-content-end flex-column">
+                                    <div className="navRight">
+                                        <select className='me-2' onChange={currencyHandler}>
+                                            <option value="usd" className='text-uppercase'>usd</option>
+                                            <option value="eur" className='text-uppercase'>eur</option>
+                                            <option value="inr" className='text-uppercase'>inr</option>
+                                        </select>
+                                        <Button variant="primary" className='text-capitalize signUp'>sign up <MdOutlineArrowOutward />
+                                        </Button>
+
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col lg={0} md={2} sm={2} xs={2} className='mnu'>
+                                <div className="navContiner my-auto" ref={menuRef}>
+                                    <GiHamburgerMenu className='mx-auto burgerMenu' onClick={() => { setSidebar(!sidebar) }} />
+                                    <nav className={sidebar ? 'sideMenu active' : 'sideMenu'}>
+                                        <ul className='sideMenuItems'>
+                                            <li className='sideMenuToggle list-unstyled mb-4 mt-3' onClick={() => { setSidebar(!sidebar) }}>
+                                                <Link to='#' className='sideBars'>
+                                                    <AiIcons.AiOutlineClose />
+                                                </Link>
+                                            </li>
+                                            {SideNavData.map((item, index) =>
+                                            (
+                                                <li key={index} className={item.cName}>
+                                                    <Link to={item.path} className='nav-link linkSidePar'>{item.title}</Link>
+                                                    <hr className='me-3'/>
+                                                </li>
+                                            )
+
+                                            )}
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </Col>
+                        </Row>
                     </Col>
+
                 </Row>
             </div>
             <hr className='mt-2' />
---
+++ src/components/navBar/SideBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+export const SideNavData = [
+    {
+    title: 
+        <p className='text-black text-capitalize sideMenuPar'>home</p>
+        ,
+    path: '/',
+    cName: 'sideBarText'
+},
+{
+    title: 
+        <p className='nav-link text-black text-capitalize sideMenuPar'>features</p>
+            ,
+    cName: 'sideBarText'
+},
+{
+    title: 
+    <p className='nav-link text-black text-capitalize sideMenuPar'>pricing</p>
+            ,
+    path: '/',
+    cName: 'sideBarText'
+},
+{
+    title: 
+    <p className='nav-link text-black text-capitalize sideMenuPar'>Blog</p>
+            ,
+    path: '/',
+    cName: 'sideBarText'
+}
+]
